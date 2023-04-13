d0ec5722b88f4578c599d8f4b0f8a1237ca7122d
Larysa Hradyska <l.v.hradyska@gmail.com>
2023-04-13 17:40:23 +0300

    adding code1st database

---
+++ ConsoleDataBase/ConsoleDataBaseContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace ConsoleDataBase
+{
+    public class ConsoleDataBaseContext : DbContext
+    {
+        private readonly string _connectionString;
+        public ConsoleDataBaseContext(DbContextOptions<ConsoleDataBaseContext> options)
+            : base(options)
+        {
+        }
+
+        public ConsoleDataBaseContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<EmployeeProject> EmployeeProjects { get; set; }
+
+        public DbSet<Office> Offices { get; set; }
+
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<Title> Titles { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeProjectConfiguration());
+            modelBuilder.ApplyConfiguration(new OfficeConfiguration());
+            modelBuilder.ApplyConfiguration(new ProjectConfiguration());
+            modelBuilder.ApplyConfiguration(new TitleConfiguration());
+        }
+    }
+}
---
+++ ConsoleDataBase/OfficeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+namespace ConsoleDataBase
+{
+    public class OfficeConfiguration : IEntityTypeConfiguration<Office>
+    {
+        public void Configure(EntityTypeBuilder<Office> builder)
+        {
+            builder
+                .ToTable("Office")
+                .HasKey(p => p.Id);
+            builder
+                .Property(p => p.Id)
+                .HasColumnName("OfficeId")
+                .ValueGeneratedNever();
+            builder.Property(p => p.Title)
+                .IsRequired()
+                .HasMaxLength(100);
+            builder.Property(p => p.Location)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
